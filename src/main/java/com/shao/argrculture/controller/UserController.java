7715dd5e4a5b4b0e8ca804644cc6e76db85428de
shaoweiwei <weishao_2016@163.com>
2017-09-01 08:57:40 +0800

    修改版本

--- src/main/java/com/shao/argrculture/controller/UserController.java
+++ src/main/java/com/shao/argrculture/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.shao.argrculture.controller;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.shao.argrculture.common.utils.JedisUtils;
+import com.shao.argrculture.common.utils.RedisUtil;
 import com.shao.argrculture.service.UserService;
 
 
@@ -14,15 +19,21 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+
+	private RedisUtil redisUtil; 
 	
       @RequestMapping("login")
       @ResponseBody
       public String getPwd(String name){
     	  String pwd = "";
+    	  String redis = "";
     	  if(name != ""){
     		  pwd = userService.selectByName(name);
+    		  redisUtil.set("pwd", pwd);
+    		  redis = (String)redisUtil.get("pwd");
+    		  
     	  }
-    	  return pwd;
+    	  return redis;
       }
       
 }
---
+++ src/test/java/test/Test.java
@@ -0,0 +1,9 @@
+package test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	 ApplicationContext app = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+}
