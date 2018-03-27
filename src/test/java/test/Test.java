18768b6e693d5eafdfe37c72eea06d4642130773
邵威威 <admin@admin-PC>
2018-03-27 09:37:45 +0800

    修改log4j输出日志文件路径

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,9 +1,9 @@
 package test;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
-	 ApplicationContext app = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-
+	public static void main(String args[]){
+		
+	}
+	
 }
